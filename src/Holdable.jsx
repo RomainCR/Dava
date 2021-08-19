cf428f063c3c5144a786650b6e45d814cd635d4c
RomainCR <romain.crey@gmail.com>
2021-08-20 00:11:42 +0200

    no move longpress

--- src/Holdable.jsx
+++ src/Holdable.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const holdTime = 500 // ms
-// const holdDistance = 3**2 // pixels squared
+const holdDistance = 3**2 // pixels squared
 
 export default function Holdable({id, onClick, onHold, children}) {
 
   const [timer, setTimer] = React.useState(null)
-  // const [pos, setPos] = React.useState([0,0])
+  const [pos, setPos] = React.useState([0,0])
 
   function onPointerDown(evt) {
-    // setPos([evt.clientX, evt.clientY]) // save position for later
+    setPos([evt.clientX, evt.clientY]) // save position for later
     const event = { ...evt } // convert synthetic event to real object
     const timeoutId = window.setTimeout(timesup.bind(null, event), holdTime)
     setTimer(timeoutId)
@@ -23,16 +23,16 @@ export default function Holdable({id, onClick, onHold, children}) {
     }
   }
 
-  // function onPointerMove(evt) {
-  //   // cancel hold operation if moved too much
-  //   if (timer) {
-  //     const d = (evt.clientX - pos[0])**2 + (evt.clientY - pos[1])**2
-  //     if (d > holdDistance) {
-  //       setTimer(null)  
-  //       window.clearTimeout(timer)
-  //     }
-  //   }
-  // }
+  function onPointerMove(evt) {
+    // cancel hold operation if moved too much
+    if (timer) {
+      const d = (evt.clientX - pos[0])**2 + (evt.clientY - pos[1])**2
+      if (d > holdDistance) {
+        setTimer(null)  
+        window.clearTimeout(timer)
+      }
+    }
+  }
 
   function timesup(evt) {
     setTimer(null)
@@ -43,7 +43,7 @@ export default function Holdable({id, onClick, onHold, children}) {
     <div
       onPointerDown={onPointerDown}
       onPointerUp={onPointerUp}
-      // onPointerMove={onPointerMove}
+      onPointerMove={onPointerMove}
       id={id}
     >
       {children}
